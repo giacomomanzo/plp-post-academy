5240f27e7fd021ee740a43910860c60bd346b1f1
giacomomanzo <giacomo.manzo99@gmail.com>
2021-11-09 11:34:37 +0100

    css only update

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -73,8 +73,6 @@ const StyledSearchButton = styled.button`
   border-radius: 5px;
   overflow: hidden;
   margin-left: 20px;
-  box-shadow: rgb(0 0 0 / 20%) 0px 2px 4px -1px,
-    rgb(0 0 0 / 14%) 0px 4px 5px 0px, rgb(0 0 0 / 12%) 0px 1px 10px 0px;
 `;
 
 const Search: React.FC<Props> = ({ value, setValue }) => {
